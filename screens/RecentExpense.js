cb1034cd5a4552c29f4f1d398c53bb533fa2af94
Burak Sevincli <115212985+buraksevincli@users.noreply.github.com>
2024-02-21 16:27:55 +0300

    Navigations and Screens Added

---
+++ screens/RecentExpense.js
@@ -0,0 +1,7 @@
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+
+const RecentExpense = () => {
+  return <ExpensesOutput expensesPeriod={"Last 7 Days"} />;
+};
+
+export default RecentExpense;
